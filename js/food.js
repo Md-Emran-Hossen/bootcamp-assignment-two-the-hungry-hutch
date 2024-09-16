51ffaee01eee0491dcf903e1d0c692f9b197371a
Md-Emran-Hossen <emran.cse21@gmail.com>
2024-09-16 12:16:10 +0600

    first commit

---
+++ js/food.js
@@ -0,0 +1,37 @@
+const loadData = (inputText)  => {
+
+    // const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=potato`;
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputText}`;
+     fetch(url)
+     .then(res =>res.json())
+     .then(data => showMeals(data.meals))
+     .catch(error => console.log("Error:", error))
+ }
+ 
+ loadData();
+ 
+ const showMeals = (meals) => {
+   
+     let container = document.getElementById("resultContainer");
+     container.innerHTML = '';
+     meals.forEach(meal => {
+         let mealCard = document.createElement('div');
+         mealCard.classList = 'card bg-base-100 shadow-xl';
+ 
+         mealCard.innerHTML = `
+         <figure class="px-5 pt-5">
+             <img src= ${meal.strMealThumb} alt= "Image of ${meal.strMeal}" class="rounded-xl" />
+         </figure>
+         <div class="card-body items-center text-center">
+             <h2 class="card-title"> 
+                  Food Name: ${meal.strMeal}!
+             </h2>
+             <p title="${meal.strInstructions}" > 
+                 Description: ${meal.strInstructions.slice(0, 150)}...
+             </p>
+            
+         </div>
+        `;
+         container.appendChild(mealCard);
+     });
+ }
