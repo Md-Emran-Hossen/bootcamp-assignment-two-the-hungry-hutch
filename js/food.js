1ecfd8fe3d0b2a0d715f27cc9e1e56e0d8167568
Md-Emran-Hossen <emran.cse21@gmail.com>
2024-09-18 00:02:31 +0600

    add modal successfully

--- js/food.js
+++ js/food.js
@@ -1,7 +1,4 @@
 
- 
-
-
 const loadData = (inputText)  => {
 
     // const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=potato`;
@@ -34,11 +31,38 @@ const loadData = (inputText)  => {
                 <span class= "font-bold" > Description: </span> ${meal.strInstructions.slice(0, 150)}...
              </p>
             <div class="card-actions justify-end">
-                <button class="btn btn-primary"> Add to Cart </button>
-            </div>
-            
+                <button onClick="handleShowDetails(${meal.idMeal})" class="btn btn-light"> 
+                    <i class="fa-solid fa-circle-info"></i>
+                    <span class = "text-xl" > Details </span>
+                </button>
+            </div>        
          </div>
         `;
          container.appendChild(mealCard);
      });
  }
+
+const handleShowDetails = (mealId) =>{
+    let url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => showMealDetails(data.meals[0]))
+        .catch(error => console.log('Error: ', error));
+}
+
+const showMealDetails = (meal) =>{
+
+    const showDetailsContainer = document.getElementById("meal-details-container");
+    showDetailsContainer.innerHTML = `
+    
+         <img class="h-96 mx-auto" src="${meal.strMealThumb}" alt="Meal Details image">
+         <p class="text-black text-2xl text-center m-5">Name: <span class="font-bold"> ${meal.strMeal} </span></p>
+         
+          <p class="text-black text-justify">
+            <span class= "text-2xl"> Description: </span>
+          ${meal.strInstructions}</p>
+    
+    `;
+
+    show_meal_Details.showModal();
+}
